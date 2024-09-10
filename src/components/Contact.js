fdc88593ae7214718f923314e34e8968e718e22c
101 <tmddnjs001133@dsm.hs.kr>
2024-09-10 23:36:18 +0900

    feat: Contact 컴포넌트 개발

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,29 @@ import styled from "styled-components";
 const ContactContainer = styled.div`
     background-color: black;
     color: white;
-    font: 200px 'Pretendard', sans-serif;
-    font-weight: 300;
+    font: 'Pretendard', sans-serif;
     padding: 2rem 0;
     text-align: center;
+    height: 1250px
+`;
+
+const ContactTitle = styled.p`
+    font-size: 200px;
+    font-weight: 300;
+`;
+
+const ContactContent = styled.p`
+    font-size: 50px;
+    font-weight: 50;
+    line-height: 1;
+`;
+
+const HoverEventTag = styled.div`
+    transition: transform 0.3s ease;
+
+    &:hover {
+        transform: scale(1.1);
+    }
 `;
 
 const Contact = () => {
@@ -15,7 +34,18 @@ const Contact = () => {
 
     return (
         <ContactContainer>
-            Contact Me!
+            <ContactTitle>
+                Contact Me
+            </ContactTitle>
+            
+            <ContactContent>
+                <HoverEventTag>DSM</HoverEventTag> <br/>
+                <HoverEventTag>2977 8517</HoverEventTag><br/>
+                <HoverEventTag>github/ori0o0p</HoverEventTag><br/>
+                <HoverEventTag>linkedin/in/ori0o0p</HoverEventTag><br/>
+                <HoverEventTag>tmddnjsrla99@gmail.com</HoverEventTag>
+            </ContactContent>
+
         </ContactContainer>
     )
 }
