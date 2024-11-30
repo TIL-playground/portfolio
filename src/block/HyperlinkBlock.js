5d8de63394a9fcb13ae49aef5c68ece9cb7e8b8b
101 <tmddnjs001133@dsm.hs.kr>
2024-11-30 10:21:30 +0900

    feat: readcv 추가

--- src/block/HyperlinkBlock.js
+++ src/block/HyperlinkBlock.js
@@ -1,50 +1,60 @@
-import github from "../imgs/GitHublogo.png";
-import linkedin from "../imgs/LinkedInlogo.png";
-import gmail from "../imgs/Gmaillogo.png";
-import velog from "../imgs/Veloglogo.png";
-import medium from "../imgs/Mediumlogo.png";
-import { HyperlinkContainer, HyperlinkImg } from '../style/Styled'
+import github_logo from "../imgs/GitHublogo.png";
+import linkedin_logo from "../imgs/LinkedInlogo.png";
+import gmail_logo from "../imgs/Gmaillogo.png";
+import velog_logo from "../imgs/Veloglogo.png";
+import medium_logo from "../imgs/Mediumlogo.png";
+import readcv_logo from "../imgs/Readcvlogo.png";
+import React from 'react';
+import { HyperlinkContainer, HyperlinkImg } from '../style/Styled';
+
+const SOCIAL_LINKS = [
+  {
+    href: "https://github.com/ori0o0p",
+    img: github_logo,
+    alt: "github"
+  },
+  {
+    href: "https://velog.io/@virtualthread",
+    img: velog_logo, 
+    alt: "velog"
+  },
+  {
+    href: "https://medium.com/@usadapekora",
+    img: medium_logo,
+    alt: "medium"
+  },
+  {
+    href: "https://www.linkedin.com/in/ori0o0p/",
+    img: linkedin_logo,
+    alt: "linkedin"
+  },
+  {
+    href: "mailto:tmddnjsrla99@gmail.com",
+    img: gmail_logo,
+    alt: "gmail"
+  },
+  {
+    href: "https://read.cv/main",
+    img: readcv_logo,
+    alt: "portfolio"
+  }
+];
 
 const HyperlinkBlock = () => {
-    return (
-        <HyperlinkContainer>
-              <a
-                href="https://github.com/ori0o0p"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <HyperlinkImg src={github} alt="github" />
-              </a>
-              <a
-                href="https://velog.io/@virtualthread"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <HyperlinkImg src={velog} alt="velog" />
-              </a>
-              <a
-                href="https://medium.com/@usadapekora"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <HyperlinkImg src={medium} alt="medium" />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/ori0o0p/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <HyperlinkImg src={linkedin} alt="linkedin" />
-              </a>
-              <a
-                href="mailto:tmddnjsrla99@gmail.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <HyperlinkImg src={gmail} alt="gmail" />
-              </a>
-        </HyperlinkContainer>
-    );
+  return (
+    <HyperlinkContainer>
+      {SOCIAL_LINKS.map(({ href, img, alt }) => (
+        <a
+          key={alt}
+          href={href}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <HyperlinkImg src={img} alt={alt} />
+        </a>
+      ))}
+    </HyperlinkContainer>
+  );
 };
 
 export default HyperlinkBlock;
